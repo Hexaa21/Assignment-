2016169ddaaac23b4ad7eedacc81256d87d6a6d4
Hexaa21 <147376504+Hexaa21@users.noreply.github.com>
2023-11-04 17:22:32 +0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] AkuSiapa = {"Kembali", "Tolong", "Please", "Comeback", "I Miss You"};
+        for(int n=0; n<=4; n++){
+            System.out.println("Untuk Cinta Ku " + n + " : " + AkuSiapa[n]);
+        }
+    }
+}
---
+++ While.java
@@ -0,0 +1,15 @@
+public class While {
+    public static void main(String[] args) {
+        int n = 2;
+
+        while( n < 10 ) {
+        System.out.println("Aku Sayang Kamu");
+        if ( n == 5 ){
+            System.out.println("Aku Benci Kamu");
+        } else {
+            System.out.println("Tolong Kembali");
+        }
+        n++;
+        }
+    }
+}
